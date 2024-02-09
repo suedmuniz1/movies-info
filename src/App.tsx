9ccd15ce1beeb9f7cf9ae51c02e61c4abed65851
Sued Nepomuceno <sued.muniz.1@gmail.com>
2024-02-09 12:09:14 -0300

    Adding Pagination component and the logic to make it work.

--- src/App.tsx
+++ src/App.tsx
@@ -2,46 +2,51 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { API_TOKEN } from "./constants/env";
 import { Header } from "./components/Header";
-
-import "./App.css";
 import { MovieCard } from "./components/MovieCard";
+import { Pagination } from "./components/Pagination";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
 
   const getMovies = async () => {
-    const moviesList = await axios
+    const response = await axios
       .get("https://api.themoviedb.org/3/movie/upcoming", {
         headers: {
           authorization: `Bearer ${API_TOKEN}`,
           accept: "application/json",
         },
         params: {
-          page: 1,
+          page: page,
           language: "en-US",
         },
       })
-      .then((response) => response.data.results);
+      .then((response) => {
+        return response.data;
+      });
 
-    setMovies(moviesList);
-    console.log(moviesList);
+    setMovies(response.results);
+    setPage(response.page);
+    setTotalPages(response.total_pages);
   };
 
   useEffect(() => {
     getMovies();
-  }, []);
+  }, [page]);
 
   return (
     <div id="root" className="bg-cyan-200 h-full font-sans">
       <div className="bg-cyan-950 text-white">
         <Header />
       </div>
-      <div className="w-3/4 mx-auto bg-cyan-50 p-10">
+      <div className="w-3/4 mx-auto bg-white p-10">
         <span className="text-3xl font-bold">Upcoming</span>
-        <div id="movies" className="flex flex-wrap justify-between">
+        <div className="grid grid-rows-5 grid-cols-4 grid-flow-row gap-4 my-10">
           {movies.map((movie: any) => {
             return (
               <MovieCard
+                key={movie.id}
                 title={movie.title}
                 overview={movie.overview}
                 posterPath={movie.poster_path}
@@ -49,6 +54,7 @@ function App() {
             );
           })}
         </div>
+        <Pagination page={page} setPage={setPage} totalPages={totalPages} />
       </div>
     </div>
   );
