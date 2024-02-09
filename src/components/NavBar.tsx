3ca060be446304baf3c7f8918328e37f2f4d533f
Sued Nepomuceno <sued.muniz.1@gmail.com>
2024-02-09 19:26:29 -0300

    Adjusting urls of links in NavBar.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,27 +1,33 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 import "./NavBar.css";
 
 export const Navbar = () => {
+  const { pathname } = useLocation();
+
   return (
     <div
       id="nav-items"
       className="text-lg h-full flex items-center justify-center"
     >
       <div className="ml-10 h-3/4 flex items-center border-b-2 border-cyan-950 nav-button">
-        <NavLink
-          to="/"
-          className="hover:text-cyan-300 transition ease delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+        <Link
+          to="/movies/popular"
+          className={`${
+            pathname.includes("movies") ? "active" : ""
+          } hover:text-cyan-300 transition ease delay-150 hover:-translate-y-1 hover:scale-110 duration-300`}
         >
           Movies
-        </NavLink>
+        </Link>
       </div>
       <div className="ml-10 h-3/4 flex items-center border-b-2 border-cyan-950 nav-button">
-        <NavLink
-          to="/series"
-          className="hover:text-cyan-300 transition ease delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+        <Link
+          to="/series/popular"
+          className={`${
+            pathname.includes("series") ? "active" : ""
+          } hover:text-cyan-300 transition ease delay-150 hover:-translate-y-1 hover:scale-110 duration-300`}
         >
           Series
-        </NavLink>
+        </Link>
       </div>
       <button className="ml-20">Project</button>
     </div>
