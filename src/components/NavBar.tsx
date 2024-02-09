fb25e7edb96f97b2e9b307939f832f5a7e16d303
Sued Nepomuceno <sued.muniz.1@gmail.com>
2024-02-09 17:25:59 -0300

    Improving stylization of NavBar.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,13 +3,26 @@ import "./NavBar.css";
 
 export const Navbar = () => {
   return (
-    <div id="nav-items" className="text-lg">
-      <NavLink to="/" className="ml-10">
-        Movies
-      </NavLink>
-      <NavLink to="/series" className="ml-10">
-        Series
-      </NavLink>
+    <div
+      id="nav-items"
+      className="text-lg h-full flex items-center justify-center"
+    >
+      <div className="ml-10 h-3/4 flex items-center border-b-2 border-cyan-950 nav-button">
+        <NavLink
+          to="/"
+          className="hover:text-cyan-300 transition ease delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+        >
+          Movies
+        </NavLink>
+      </div>
+      <div className="ml-10 h-3/4 flex items-center border-b-2 border-cyan-950 nav-button">
+        <NavLink
+          to="/series"
+          className="hover:text-cyan-300 transition ease delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+        >
+          Series
+        </NavLink>
+      </div>
       <button className="ml-20">Project</button>
     </div>
   );
