fbcfb5d8fe1451e15d5f71a0d54b6b75aae46be2
Sued Nepomuceno <sued.muniz.1@gmail.com>
2024-02-10 18:41:44 -0300

    Adding new constant files for movies and series nav bar.

---
+++ src/constants/moviesNavBar.ts
@@ -0,0 +1,18 @@
+export const moviesOptions: Record<string, string>[] = [
+  {
+    name: "Popular",
+    path: "/movies/popular",
+  },
+  {
+    name: "Upcoming",
+    path: "/movies/upcoming",
+  },
+  {
+    name: "Top Rated",
+    path: "/movies/top-rated",
+  },
+  {
+    name: "Now Playing",
+    path: "/movies/now-playing",
+  },
+];
---
+++ src/constants/seriesNavBar.ts
@@ -0,0 +1,14 @@
+export const seriesOptions: Record<string, string>[] = [
+  {
+    name: "Popular",
+    path: "/series/popular",
+  },
+  {
+    name: "Top Rated",
+    path: "/series/top-rated",
+  },
+  {
+    name: "On the air",
+    path: "/series/on-the-air",
+  },
+];
