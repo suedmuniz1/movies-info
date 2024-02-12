633ec08560e1cd5145f35bb52d2617b92b914bbc
Sued Nepomuceno <sued.muniz.1@gmail.com>
2024-02-12 15:20:42 -0300

    Adding i18n initial structure to main.tsx and creating the translations files.

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+import en from "./services/i18n/en.json";
+import pt from "./services/i18n/pt.json";
+
+i18next.use(initReactI18next).init({
+  lng: "en",
+  debug: true,
+  resources: {
+    en,
+    pt,
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
