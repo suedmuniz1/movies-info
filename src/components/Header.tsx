0e71b1716253942a521d8206160d9c13b3f6f128
Sued Nepomuceno <sued.muniz.1@gmail.com>
2024-02-12 03:18:44 -0300

    Adjusting positioning.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <div
       id="header-container"
-      className="w-3/4 mx-auto px-5 h-16 flex justify-between items-center"
+      className="mx-auto h-16 flex justify-between items-center"
     >
       <button
         id="logo"
